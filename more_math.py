f89bbaa67bde2a84063b680e298ce487da22195b
hh6261 <hh6261@g.rit.edu>
2025-09-20 18:25:25 +0200

    COMMIT

--- more_math.py
+++ more_math.py
@@ -1,13 +1,40 @@
 '''
-@ASSESSME.USERID: userID
-@ASSESSME.AUTHOR: author, list of authors
+@ASSESSME.USERID: hh6261
+@ASSESSME.AUTHOR: Hana Hebib
 @ASSESSME.DESCRIPTION: Assignment 3.1
 @ASSESSME.ANALYZE: YES
 '''
 
+import math
+
+def fact(b):
+    if b > 0 and isinstance(n, int):
+        return math.factorial(b)
+    else:
+        return 0
+    
+
+def root (b1):
+    if b1 > 0:
+        return math.sqrt(float(b1))
+    else:
+        return 0
+    
+def trunk(b2):
+    return math.trunc(b2)
+
+
 
 def main():
-   pass
+   b = int(input("Enter a numeric value: "))
+   print(b,"factorial=", fact(b))
+
+   b1 = float(input("Enter a numeric value: "))
+   print("The square root of",b1,"=",root(b1))
+
+   b2 = float(input("Enter a numeric value: "))
+   print(b2,"truncated:", trunk(b2))
+
 
 
 if __name__ == "__main__":
